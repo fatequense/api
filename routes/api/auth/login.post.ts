71b92a7b2e97a982951632e146f94bb00a2ac53d
sads4ndwich <caiocamargo007@gmail.com>
2023-10-14 19:22:03 -0300

    feat: login with siga account

---
+++ routes/api/auth/login.post.ts
@@ -0,0 +1,30 @@
+import { object, parse, string } from "valibot";
+import { COOKIES, GX_STATE, INPUT_IDS, STATUS } from "~/core/constants";
+import { api } from "~/core/network";
+
+const loginSchema = object({
+  username: string(),
+  password: string()
+});
+
+export default defineEventHandler(async event => {
+  const { username, password } = parse(loginSchema, await readBody(event));
+
+  const { res } = await api.post("/aluno/login.aspx", {
+    data: {
+      GXState: GX_STATE.LOGIN,
+      [INPUT_IDS.USERNAME]: username,
+      [INPUT_IDS.PASSWORD]: password,
+      [INPUT_IDS.CONFIRM]: "Confirmar"
+    }
+  });
+
+  if (res.status !== STATUS.REDIRECT) throw new Error("Failed to login");
+
+  const cookies = parseCookie(res.headers['set-cookie'] as string);
+  const token = jwtSign({
+    session: cookies.get(COOKIES.AUTH) as string
+  });
+
+  return { token };
+});
