fceaed8ab86acc736927581cfb7e9b3cde9e81da
sads4ndwich <caiocamargo007@gmail.com>
2023-10-17 16:54:43 -0300

    chore: set custom error handler

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,9 @@
+import errorHandler from './errors';
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
+  errorHandler: "~/errors",
+  devErrorHandler: errorHandler,
   storage: {
     cache: {
       driver: "redis",
