cc6ea4fee910f6953acd3f1d3f2bb770c9ffc22a
sads4ndwich <caiocamargo007@gmail.com>
2023-10-17 17:22:16 -0300

    feat: added logout route

---
+++ routes/api/auth/logout.post.ts
@@ -0,0 +1,35 @@
+import { COOKIES, STATUS } from "~/core/constants";
+import { api } from "~/core/network";
+import { parseHtml } from "~/core/scrap";
+import { AccessDeniedError } from "~/errors/exceptions/siga.error";
+
+async function getLogoutGXStateEvent(token: string) {
+	const { res } = await api.get("/aluno/home.aspx", token);
+  
+  if (res.status === STATUS.REDIRECT) throw new AccessDeniedError();
+
+  const gxstate = await parseHtml(res.data);
+
+	return JSON.stringify({
+    ...gxstate.parsed,
+    _EventName: `${gxstate.prefix}E'SAIR'.`,
+    sCallerURL: 'http://siga.cps.sp.gov.br/login.aspx',
+  });
+}
+
+export default defineEventHandler(async event => {
+	const logoutEvent = await getLogoutGXStateEvent(event.context.token)
+
+	const { res } = await api.post("/aluno/home.aspx", {
+		headers: {
+			cookie: `${COOKIES.AUTH}=${decrypt(event.context.token)}`
+		},
+		data: {
+			GXState: logoutEvent
+		}
+	});
+
+	if (res.statusCode !== STATUS.REDIRECT) throw new AccessDeniedError();
+
+	return { ok: true };
+});
