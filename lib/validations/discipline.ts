e131213e9c82529c045f68556288ecaea0c35534
sads4ndwich <caiocamargo007@gmail.com>
2023-11-06 17:43:55 -0300

    feat: added discipline validations

--- lib/validations/discipline.ts
+++ lib/validations/discipline.ts
@@ -1,6 +1,29 @@
-import { Output, number, object, string, transform } from "valibot";
+import { Output, array, number, object, omit, pick, string, transform } from "valibot";
 
 export const disciplineSchema = object({
+  name: string(),
+  code: string(),
+  class: string(),
+  teacherName: transform(
+    string(),
+    teacherName => teacherName
+      .toLowerCase()
+      .replace(/(^|\s)\S/g, (char) => char.toUpperCase())
+  ),
+  courseProgram: string(),
+  goal: string(),
+  workload: object({
+    weekly: number(),
+    teorical: number(),
+    practical: number(),
+    total: number(),
+  }),
+  totalAbsencesAllowed: number()
+})
+
+export type Discipline = Output<typeof disciplineSchema>;
+
+export const disciplineScheduleSchema = object({
   name: string(),
   code: string(),
   class: string(),
@@ -14,4 +37,13 @@ export const disciplineSchema = object({
   totalAbsencesAllowed: number()
 })
 
-export type Discipline = Output<typeof disciplineSchema>;
+export type DisciplineSchedule = Output<typeof disciplineScheduleSchema>;
+
+export const disciplineMinifySchema = pick(
+  disciplineSchema,
+  ['name', 'code']
+)
+
+export type DisciplineMinify = Output<typeof disciplineMinifySchema>;
+
+export const disciplinesMinifySchema = array(disciplineMinifySchema);
