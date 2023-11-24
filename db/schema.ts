52667680b7afb9324861916665217a92f7b3aef0
sads4ndwich <caiocamargo007@gmail.com>
2023-11-24 11:37:18 -0300

    feat: added activity schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { boolean, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const students = pgTable('students', {
 	id: varchar('id', { length: 13 }).primaryKey(),
@@ -7,3 +8,16 @@ export const students = pgTable('students', {
 
 export type Student = typeof students.$inferSelect;
 export type NewStudent = typeof students.$inferInsert;
+
+export const activities = pgTable('activities', {
+	id: uuid('id').primaryKey().default(sql`gen_random_uuid()`),
+	studentId: varchar('studentId').references(() => students.id),
+	disciplineCode: varchar('disciplineCode', { length: 6 }),
+	title: varchar('title', { length: 64 }),
+	description: varchar('description', { length: 255 }),
+	untilAt: timestamp('untilAt'),
+	isCompleted: boolean('isCompleted'),
+});
+
+export type Activity = typeof activities.$inferSelect;
+export type NewActivity = typeof activities.$inferInsert;
